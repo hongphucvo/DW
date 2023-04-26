827ad5820bf6fc24253b34bbad64de2cc8616097
hongphucvo <vohongphuc2001@gmail.com>
2023-04-26 10:10:39 +0700

    init

---
+++ dss/api/models.py
@@ -0,0 +1,36 @@
+# from django.db import models
+
+# # Create your models here.
+# from django.db import models
+# # from django.contrib.auth.models import User
+
+# class Customer(models.Model):
+#     loan_amnt = models.FloatField()
+#     term = models.JSONField()
+#     int_rate = models.FloatField()
+#     installment = 
+#     grade 
+#     sub_grade
+#     emp_title
+#     emp_length
+#     home_ownership
+#     annual_inc
+#     verification_status
+#     isue_d
+#     loan_status
+#     purpose 
+#     title
+#     DoesNotExistearliest_cr_line
+#     open_acc
+#     pub_rec
+#     revol_bal
+#     revol_util
+#     total_acc
+#     initial_list_status
+#     application_type
+
+#     task = models.CharField(max_length = 180)
+    
+
+#     def __str__(self):
+#         return self.task
