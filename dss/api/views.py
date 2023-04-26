e9426599bb7549b80f0c512e0cef49ae3a37c824
hongphucvo <vohongphuc2001@gmail.com>
2023-04-26 13:01:16 +0700

    indexing req

--- dss/api/views.py
+++ dss/api/views.py
@@ -13,25 +13,30 @@ class CustomerListApiView(APIView):
 
     # 0. List all
     def get(self, request, *args, **kwargs):
-        '''
-        List all the todo items for given requested user
-        '''
-        # todos = []
-        # serializer = CustomerSerializer(todos, many=True)
-        data = pd.read_csv('''../model/btl_dw.csv''',header=0).drop(['loan_repaid'],axis=1)
+        data = pd.read_csv('''../dss/api/model/btl_dw.csv''',header=0).iloc[:, 0:10]
         return Response(data.to_dict(orient='records'), status=status.HTTP_200_OK)
         # return Response({"msg":'This is DSS Pay loan'}, status=status.HTTP_200_OK)
-
         
     # 1. Predict
     def post(self, request, *args, **kwargs):
-        data = request.data
-        model = load_model('../model/full_data_project_model.h5')
-        x_test = pd.DataFrame(data=data)
-        if len(data)<1:
+        arr = request.data.get("arr")
+        if len(arr)<1:
             return Response('Can not predict', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        pred = model.predict(x_test.drop(['loan_repaid'],axis=1).values.reshape(x_test.shape[0],78))
+        model = load_model('../dss/api/model/full_data_project_model.h5')
+        data=get_user(arr)
+        pred = model.predict(data)
         result=list(map(lambda prediction: prediction[0], pred))
         return Response(result, status=status.HTTP_200_OK)
+    
+        # data = request.data
+        # model = load_model('../model/full_data_project_model.h5')
+        # x_test = pd.DataFrame(data=data)
+        # if len(data)<1:
+        #     return Response('Can not predict', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # pred = model.predict(x_test.drop(['loan_repaid'],axis=1).values.reshape(x_test.shape[0],78))
+        # result=list(map(lambda prediction: prediction[0], pred))
+        # return Response(result, status=status.HTTP_200_OK)
         # return Response({'predictions':result}, status=status.HTTP_200_OK)
-   
+def get_user(arr):
+    data = pd.read_csv('''../dss/api/model/btl_dw.csv''',header=0).drop(['loan_repaid'],axis=1).loc[arr]
+    return data.values.reshape(data.shape[0],78)
